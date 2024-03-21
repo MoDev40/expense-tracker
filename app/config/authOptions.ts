349405055fa4615a29b037a46389408149c51e49
M40 <modev.404@gmail.com>
2024-03-22 00:07:27 +0300

    user auth sign-up

---
+++ app/config/authOptions.ts
@@ -0,0 +1,62 @@
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+import  connectDB  from "./connectDB";
+import UserModel from "../models/UserModel";
+
+const GOOGLE_CLIENT_ID = process.env.AUTH_GOOGLE_CLIENT_ID!
+const GOOGLE_CLIENT_SECRET = process.env.AUTH_GOOGLE_CLIENT_SECRET!
+
+export const authOptions : NextAuthOptions = {
+    session:{
+        strategy:"jwt"
+    },
+    providers:[
+        GoogleProvider({
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,        
+        })
+    ],
+    callbacks:{
+        async signIn({profile}){
+
+            if (!profile?.email) {
+                throw new Error('No profile')
+            }
+
+            try {
+                connectDB()
+                await UserModel.updateOne(
+                  { email: profile.email },
+                  { $set: { email: profile.email, name: profile.name, avatar: profile.image } },
+                  { upsert: true }
+                );
+              } catch (error) {
+                console.error('Error updating document:', error);
+              }
+              
+            return true
+        },async jwt({token}){
+            if(!token.email){
+                return{
+                    message:"Invalid credentials"
+                }
+            }
+            connectDB()
+            const currentUser = await UserModel.findOne({ email: token.email });
+            return{
+                ...token,
+                id:currentUser?._id,
+                name:currentUser?.name,
+                email:currentUser?.email,
+            }
+        },session({token,session}){
+            return {
+                    ...session,
+                    user: {
+                      ...session.user,
+                      id: token.id,
+                    }, 
+                }
+            }
+    }
+} 
