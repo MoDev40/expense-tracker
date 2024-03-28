ea8b7ba1d705d1352d679e5cd7938d19fe784ae4
M40 <modev.404@gmail.com>
2024-03-28 15:29:13 +0300

    remove try catch

--- app/actions/expense.action.ts
+++ app/actions/expense.action.ts
@@ -6,38 +6,26 @@ import connectDB from "../config/connectDB";
 
 
 export async function createExpense(data:ExpenseBody) : Promise<void>{
-    try {
-        connectDB()
-        const newExpense = {
-            amount: data.amount,
-            user:data.user,
-            tag:data.tag_id   
-        }
-        const createdExp = new ExpenseModel(newExpense)
-        createdExp.save();
-    } catch (error) {
-        console.log(error);
+    connectDB()
+    const newExpense = {
+        amount: data.amount,
+        user:data.user,
+        tag:data.tag_id   
     }
-    
+    const createdExp = new ExpenseModel(newExpense)
+    createdExp.save();
 }
 
 export async function deleteMyExpense(id:string) : Promise<void> {
-    try {
-        connectDB()
-        await ExpenseModel.findByIdAndDelete({_id:id})
-    } catch (error) {
-        console.log(error);
-    }
+    connectDB()
+    await ExpenseModel.findByIdAndDelete({_id:id})
+
 }
 
 export async function updateMyExpense({expense_id,data}:{expense_id:string,data:ExpenseBody}) : Promise<void> {
-    try {
-        connectDB();
-        await ExpenseModel.findByIdAndUpdate({_id:expense_id},{$set:{
-            amount:data.amount,
-            tag:data.tag_id,
-        }})
-    } catch (error) {
-        console.log(error);
-    }
+    connectDB();
+    await ExpenseModel.findByIdAndUpdate({_id:expense_id},{$set:{
+        amount:data.amount,
+        tag:data.tag_id,
+    }})
 }
